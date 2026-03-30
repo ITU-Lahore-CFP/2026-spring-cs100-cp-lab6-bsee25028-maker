538c9701028d762374e6cd69ad12b7929066791d
bsee25028-maker <bsee25028@itu.edu.pk>
2026-03-30 11:01:17 +0500

    BSEE-25028 LAB 7 complete

--- functions.cpp
+++ functions.cpp
@@ -1,12 +1,27 @@
 #include "functions.h"
 
 
+
 int sumOfSquares(int n) 
-{
+{ 
+    int sum = 0;
+    for (int i = 1; i <= n; i++) { // Iterate from 1 to n and calculate the sum of squares
+        sum += i * i;// Recursive case: add the square of n to the sum of squares of (n-1)
+    }
+    return sum;  // Base case: if n is 0, return 0  
+
 }
 
 int findFactorial(int n)
 {
+    if (n == 0) {
+        return 1;
+    }
+    int factorial = 1; // Initialize factorial to 1
+    for (int i = 1; i <= n; i++) { // Iterate from 1 to n and calculate the factorial
+        factorial *= i;
+    } // Recursive case: multiply n by the factorial of (n-1)
+    return factorial; // Base case: if n is 0, return 1
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
 #include "functions.h"
 
 
-int main() {
-   
+int main() 
+{
+    int choice;
+
+    cout << "----- MENU -----" << endl;
+    cout << "1. Sum of Squares" << endl; // Display the menu options to the user
+    cout << "2. Factorial" << endl;
+    cout << "3. Exit" << endl;
+    cout << "Enter your choice: "; // the user to enter their choice for the menu options
+    cin >> choice;
+
+    switch(choice)
+    {
+        case 1:
+        {
+            int n;
+            cout << "Enter a number: ";// the user to enter a number for calculating the sum of squares
+            cin >> n;
+
+            cout << "Sum of squares from 1 to " << n // Output the result of sum of squares for the given number
+                 << " is: " << sumOfSquares(n) << endl;  
+            break; // Output the result of sum of squares for the given number
+        }
+
+        case 2:
+        {
+            int num;
+            cout << "Enter a number to find its factorial: ";  // the user to enter a number for calculating the factorial
+            cin >> num;
+
+            cout << "Factorial of " << num  // Output the result of factorial for the given number
+                 << " is: " << findFactorial(num) << endl;  // Output the result of factorial for the given number
+            break;
+        }
+
+        case 3:
+        {
+            cout << "Program exited." << endl; // Output a message indicating that the program has exited
+            break;
+        }
+
+        default:
+        {
+            cout << "Invalid choice!" << endl; // Output a message indicating that the user has entered an invalid choice
+        }
+    }
+
+    return 0;
 }
